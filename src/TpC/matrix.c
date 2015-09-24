2024e40256d7504da1953bac3f6ed14329e221df
root <root@nick-Note>
2015-09-24 04:04:51 -0300

    Modificando el main tpv2.c con getopt para lectura de consola Agregando la impresion por consola, por cada linea. tp.c tambien anda en ese modo. *para probar tpv2.c descomentar **No toque la version en Mips32

--- src/TpC/matrix.c
+++ src/TpC/matrix.c
@@ -1,58 +1,83 @@
 #include "matrix.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <unistd.h>
 unsigned char proximo(unsigned char *a,
-					  unsigned int i, unsigned int j,
-					  unsigned char regla, unsigned int N);
+        unsigned int i, unsigned int j,
+        unsigned char regla, unsigned int N);
 
 void Matrix_create(Matrix* self, size_t size) {
-	self->ptr = malloc(size*size);
-	self->size = size;
+    self->ptr = malloc(size * size);
+    self->size = size;
 }
-
 
 void Matrix_write(Matrix* self, unsigned char value, size_t row, size_t column) {
-	self->ptr[self->size*row + column] = value;
+    self->ptr[self->size * row + column] = value;
 }
 
-char Matrix_read(Matrix* self, size_t row, size_t column){
-	return self->ptr[self->size*row + column];
+char Matrix_read(Matrix* self, size_t row, size_t column) {
+    return self->ptr[self->size * row + column];
 }
-
 
 void Matrix_destroy(Matrix* self) {
-	free(self->ptr);
+    free(self->ptr);
 }
 
-void Extend(Matrix* self, unsigned char rule) {
-	for (unsigned int i = 0; i < self->size-1; ++i) {
-		for (unsigned int j = 0; j < self->size; ++j ) {
-			unsigned char value = proximo(self->ptr, i, j, rule, self->size);
-			Matrix_write(self, value, i+1, j);
-		}
-	}
+void Print_Matrix(Matrix* self) {
+    for (unsigned int i = 0; i < self->size - 1; ++i) {
+        for (unsigned int j = 0; j < self->size; ++j) {
+            fprintf(stdout, "%d", Matrix_read(self, i, j));
+        }
+        printf(" \n");
+    }
+}
+// se observa mejor por linea que por matriz entera
+void Print_Linea(Matrix* self,int i) {
+    
+        for (unsigned int j = 0; j < self->size; ++j) {
+            fprintf(stdout, "%d", Matrix_read(self, i, j));
+        }
+        printf(" \n");
+    
+}
+
+void Extend(Matrix* self, unsigned char rule, int terminal) {
+    
+    for (unsigned int i = 0; i < self->size - 1; ++i) {
+        for (unsigned int j = 0; j < self->size; ++j) {
+            unsigned char value = proximo(self->ptr, i, j, rule, self->size);
+            Matrix_write(self, value, i + 1, j);
+        }
+        if (terminal == 1) {
+            // printf("\033[H\033[J");
+            Print_Linea(self,i);
+            // Print_Matrix(self);
+            sleep(1);
+        }
+    }
 }
 
 unsigned char proximo(unsigned char *a,
-					  unsigned int i, unsigned int j,
-					  unsigned char regla, unsigned int N) {
-	unsigned char left, right, actual;
-	if (j > 0) {
-		left = *(a + i*N + j-1);
-	} else {
-		left = *(a + i*N + N-1);
-	}
-	if (j != N-1) {
-		right = *(a + i*N + j+1);
-	} else {
-		right = *(a + i*N);
-	}
-	actual = *(a + i*N + j);
+        unsigned int i, unsigned int j,
+        unsigned char regla, unsigned int N) {
+    unsigned char left, right, actual;
+    if (j > 0) {
+        left = *(a + i * N + j - 1);
+    } else {
+        left = *(a + i * N + N - 1);
+    }
+    if (j != N - 1) {
+        right = *(a + i * N + j + 1);
+    } else {
+        right = *(a + i * N);
+    }
+    actual = *(a + i * N + j);
 
-	int pos = right + actual*2 + left*4;
-	unsigned char value = (regla << (8 - pos - 1));
-	value = (value >> (8 - 1));
+    int pos = right + actual * 2 + left * 4;
+    unsigned char value = (regla << (8 - pos - 1));
+    value = (value >> (8 - 1));
 
-	return value;
+    return value;
 }
 
--- src/TpC/tp.c
+++ src/TpC/tp.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +15,7 @@
 #define VERSIONCODE "-V"
 #define VERSIONCODE_LONG "--version"
 
+
 void writePbmImage(Matrix* matrix, unsigned int n, FILE *file);
 
 int loadMatrix(Matrix* matrix, FILE* input);
@@ -21,118 +24,124 @@ void printHelpMenu();
 
 int main(int argc, char** argv) {
 
-	if (argc < 2) {
-		printHelpMenu();
-		return EXIT_FAILURE;
-	}
+    if (argc < 2) {
+        printHelpMenu();
+        return EXIT_FAILURE;
+    }
 
-	FILE *fileInput;
-	FILE *fileOutput;
-	unsigned char rule;
-	unsigned int size;
-	char help[] = HELPCODE;
-	char helpLong[] = HELPCODE_LONG;
-	char version[] = VERSIONCODE;
-	char versionLong[] = VERSIONCODE_LONG;
+    FILE *fileInput;
+    FILE *fileOutput;
+    unsigned char rule;
+    unsigned int size;
+    char help[] = HELPCODE;
+    char helpLong[] = HELPCODE_LONG;
+    char version[] = VERSIONCODE;
+    char versionLong[] = VERSIONCODE_LONG;
 
-	if ( !strcmp(argv[1], help) || !strcmp(argv[1], helpLong) ) {
-		printHelpMenu();
-		return EXIT_SUCCESS;
-	} else if ( !strcmp(argv[1], version) || !strcmp(argv[1], versionLong) ) {
-		printf("Autcel Version C\n");
-		return EXIT_SUCCESS;
-	}
+    if (!strcmp(argv[1], help) || !strcmp(argv[1], helpLong)) {
+        printHelpMenu();
+        return EXIT_SUCCESS;
+    } else if (!strcmp(argv[1], version) || !strcmp(argv[1], versionLong)) {
+        printf("Autcel Version C\n");
+        return EXIT_SUCCESS;
+    }
+    rule = atoi(argv[1]);
+    size = atoi(argv[2]);
 
-	rule = atoi(argv[1]);
-	size = atoi(argv[2]);
+    fileInput = fopen(argv[3], "r");
+    if (fileInput == NULL) {
+        printf("Error en el archivo de entrada\n");
+        return EXIT_FAILURE;
+    }
 
-	Matrix matrix;
-	Matrix_create(&matrix, size);
+    Matrix matrix;
+    Matrix_create(&matrix, size);
 
-	fileInput = fopen( argv[3], "r");
-	printf("Leyendo estado inicial...\n");
-	if ( loadMatrix(&matrix, fileInput) == EXIT_FAILURE) {
-		fprintf(stderr,"%s","Archivo de entrada erroneo\n");
-		Matrix_destroy(&matrix);
-		return EXIT_FAILURE;
-	}
+    printf("Leyendo estado inicial...\n");
+    if (loadMatrix(&matrix, fileInput) == EXIT_FAILURE) {
+        fprintf(stderr, "%s", "Archivo de entrada erroneo\n");
+        Matrix_destroy(&matrix);
+        return EXIT_FAILURE;
+    }
 
-	char* fileOutputName;
-	if (argc > 4) {
-		fileOutputName = argv[5];
-		fileOutput = fopen(strcat(argv[5],".pbm"), "w+");
-	} else {
-		fileOutputName = argv[3];
-		fileOutput = fopen(strcat(argv[3],".pbm"), "w+");
-	}
+    char* fileOutputName;
+    int terminal = 0;
+    if (argc > 5) {
+        fileOutputName = argv[5];
+        fileOutput = fopen(strcat(argv[5], ".pbm"), "w+");
+    } else {
+        // fileOutputName = argv[3];
+        // fileOutput = fopen(strcat(argv[3],".pbm"), "w+");
+        terminal = 1;
 
-	Extend(&matrix, rule);
-	printf("Grabando %s\n",fileOutputName);
-	writePbmImage(&matrix, size, fileOutput);
-	printf("Listo\n");
+    }
+    Extend(&matrix, rule, terminal);
+    if (terminal == 0) {
+        printf("Grabando %s\n", fileOutputName);
+        writePbmImage(&matrix, size, fileOutput);
+        printf("Listo\n");
+        fclose(fileOutput);
+    }
+    Matrix_destroy(&matrix);
+    fclose(fileInput);
 
-	Matrix_destroy(&matrix);
-	fclose(fileInput);
-	fclose(fileOutput);
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+
 }
-
 
 void writePbmImage(Matrix* matrix, unsigned int n, FILE *file) {
-	int width = PIXELWIDTH;
-	int height = PIXELHEIGHT;
+    int width = PIXELWIDTH;
+    int height = PIXELHEIGHT;
 
-	fprintf(file, "%s\n", MAGICNUMBER);						// Header
-	fprintf(file, "%d %d\n", n*PIXELWIDTH, n*PIXELHEIGHT); 	// Width and Height
+    fprintf(file, "%s\n", MAGICNUMBER); // Header
+    fprintf(file, "%d %d\n", n*PIXELWIDTH, n * PIXELHEIGHT); // Width and Height
 
-	int i, pixY, j, pixX;
-	for (i = 0; i < n; ++i) {							// Writing the image
-		for (pixY = 0; pixY < height; ++pixY) {
-			for (j = 0; j < n; ++j) {
-				for (pixX = 0; pixX < width; ++pixX) {
-					unsigned char value = Matrix_read(matrix, i, j);
-					fprintf(file, "%d ", value);
-				}
-			}
-			putc('\n',file);
-		}
-	}
+    int i, pixY, j, pixX;
+    for (i = 0; i < n; ++i) { // Writing the image
+        for (pixY = 0; pixY < height; ++pixY) {
+            for (j = 0; j < n; ++j) {
+                for (pixX = 0; pixX < width; ++pixX) {
+                    unsigned char value = Matrix_read(matrix, i, j);
+                    fprintf(file, "%d ", value);
+                }
+            }
+            putc('\n', file);
+        }
+    }
 }
-
 
 int loadMatrix(Matrix* matrix, FILE* input) {
-	int j;
-	for(j = 0; j < matrix->size; ++j) {
-		char c = fgetc(input);
-		unsigned char cell = atoi(&c);
-		if (cell != 0 && cell != 1 ) {
-			return 1;
-		}
-		Matrix_write(matrix, cell, 0, j);
-	}
-	fgetc(input);
-	if (fgetc(input) != EOF) {
-		return 1;
-	}
-	return 0;
+    int j;
+    for (j = 0; j < matrix->size; ++j) {
+        char c = fgetc(input);
+        unsigned char cell = atoi(&c);
+        if (cell != 0 && cell != 1) {
+            return 1;
+        }
+        Matrix_write(matrix, cell, 0, j);
+    }
+    fgetc(input);
+    if (fgetc(input) != EOF) {
+        return 1;
+    }
+    return 0;
 }
 
-
 void printHelpMenu() {
-	printf("Uso: \n");
-	printf("\tautcel -h\n");
-	printf("\tautcel -V \n");
-	printf("\tautcel R N inputfile [-o outputprefix]\n");
-	printf("Opciones: \n");
-	printf("\t-h, --help	Imprime este mensaje.\n");
-	printf("\t-V, --version	Da la version del programa.\n");
-	printf("\t-o  Prefijo de los archivos de salida.\n");
-	printf("Ejemplos:\n");
-	printf("\tautcel 30 80 inicial -o evolucion\n");
-	printf("\tCalcula la evolucion del automata Regla 30\n");
-	printf("\ten un mundo unidimensional de 80 celdas, por 80 iteraciones.\n");
-	printf("\tEl archivo de salida se llamara evolucion.pbm.\n");
-	printf("\tSi no se da un prefijo para los archivos de salida,\n");
-	printf("\tel prefijo sera el nombre del archivo de entrada.\n");
+    printf("Uso: \n");
+    printf("\tautcel -h\n");
+    printf("\tautcel -V \n");
+    printf("\tautcel R N inputfile [-o outputprefix]\n");
+    printf("Opciones: \n");
+    printf("\t-h, --help	Imprime este mensaje.\n");
+    printf("\t-V, --version	Da la version del programa.\n");
+    printf("\t-o  Prefijo de los archivos de salida.\n");
+    printf("Ejemplos:\n");
+    printf("\tautcel 30 80 inicial -o evolucion\n");
+    printf("\tCalcula la evolucion del automata Regla 30\n");
+    printf("\ten un mundo unidimensional de 80 celdas, por 80 iteraciones.\n");
+    printf("\tEl archivo de salida se llamara evolucion.pbm.\n");
+    printf("\tSi no se da un prefijo para los archivos de salida,\n");
+    printf("\tel prefijo sera el nombre del archivo de entrada.\n");
 }
 
